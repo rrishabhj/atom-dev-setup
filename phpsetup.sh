f5c9d6010ea5e2434b710f46f67f5439eba92fe0
Rishabh <rishabhjindal4@gmail.com>
2017-04-05 01:26:12 +0530

    php setup

---
+++ phpsetup.sh
@@ -0,0 +1,12 @@
+
+# php setup
+
+# Run the 'PHP Coding Standards Fixer' within Atom
+apm install php-cs-fixer
+# Lint PHP on the fly, using php -l
+apm install linter-php
+apm install linter-phpmd
+# Hyperclick provider for PHP. Open file when classname is clicked
+apm install php-hyperclick
+
+apm install aligner-php
